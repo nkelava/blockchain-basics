84c562c29164a1fd49a96b7fc0d42b243246f18e
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-03 21:21:28 +0100

    Add fund me deployment

--- hardhat/fund-me/deploy/01-deploy-fund-me.js
+++ hardhat/fund-me/deploy/01-deploy-fund-me.js
@@ -1,9 +1,31 @@
-// function deploy(hre) {
-//   console.log("Hi");
-// }
+const { network } = require("hardhat");
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config");
 
-// module.exports.default = deploy;
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let ethUsdPriceFeedAddress;
+
+  log("Deploying FundMe contract...");
+
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeedAddress;
+  }
+
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: [ethUsdPriceFeedAddress],
+    log: true,
+  });
+  log("FundMe deployed...");
+  log("--------------------------------------------------------------------");
 };
+
+module.exports.tags = ["all", "fundme"];
