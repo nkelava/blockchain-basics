a18923dace2d057bd5e515c7494c9164ce33aa79
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-05 21:10:45 +0100

    Add mock deploy

--- hardhat/lottery/server/deploy/00-deploy-mocks.js
+++ hardhat/lottery/server/deploy/00-deploy-mocks.js
@@ -1,27 +1,23 @@
 const { network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config");
+
+const BASE_FEE = "250000000000000000";
+const GAS_PRICE_LINK = 1e9;
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
-  if (developmentChains.includes(network.name)) {
+  if (network.config.chainId == 31337) {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts();
-    const chainId = network.config.chainId;
 
-    if (chainId === 31337) {
-      log("Local network detected! Deploying mocks...");
+    log("Local network detected! Deploying mocks...");
 
-      await deploy("MockV3Aggregator", {
-        contract: "MockV3Aggregator",
-        from: deployer,
-        log: true,
-        args: [DECIMALS, INITIAL_ANSWER],
-      });
+    await deploy("VRFCoordinatorV2Mock", {
+      from: deployer,
+      log: true,
+      args: [BASE_FEE, GAS_PRICE_LINK],
+    });
 
-      log("Mocks deployed...");
-      log(
-        "--------------------------------------------------------------------"
-      );
-    }
+    log("Mocks deployed...");
+    log("------------------------------------------------------------------");
   }
 };
 
