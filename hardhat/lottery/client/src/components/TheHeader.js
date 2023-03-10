c4b15f88e1d62f56cf547465f31ba58d4ca5a2ec
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-10 02:22:02 +0100

    Update style

--- hardhat/lottery/client/src/components/TheHeader.js
+++ hardhat/lottery/client/src/components/TheHeader.js
@@ -4,7 +4,7 @@ import styles from "@/styles/Header.module.css";
 export default function TheHeader() {
   return (
     <div className={styles.header}>
-      <div className={styles.header__brand}>Decentralized Raffle</div>
+      <div className={styles.header__brand}>DEECNTRALIZED RAFFLE</div>
       <ConnectButton
         moralisAuth={false}
         className={styles.header__connectBtn}
