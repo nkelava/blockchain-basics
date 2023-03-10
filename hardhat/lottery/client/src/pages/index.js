86c86444fc208053a6a0a16df439423dbb1fe15b
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-10 02:21:34 +0100

    Update component imports

--- hardhat/lottery/client/src/pages/index.js
+++ hardhat/lottery/client/src/pages/index.js
@@ -2,8 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import TheHeader from "@/components/TheHeader";
-// import ManualHeader from "@/components/ManualHeader";
+import { TheHeader, TheRaffleEntrance } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +18,7 @@ export default function Home() {
       {/* <ManualHeader /> */}
       <main className={styles.main}>
         <TheHeader />
-        <h1>Welcome!</h1>
+        <TheRaffleEntrance />
       </main>
     </div>
   );
