7d3f9a0a4615f9653521bd3ce7c2c835c9818b8f
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-10 02:21:20 +0100

    Add export index file for components

---
+++ hardhat/lottery/client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as TheManualHeader } from "@/components/TheManualHeader";
+export { default as TheHeader } from "@/components/TheHeader";
+export { default as TheRaffleEntrance } from "@/components/TheRaffleEntrance";
