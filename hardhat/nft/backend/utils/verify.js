f18c65835b5d63efc81a20bffca9f973516c5608
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-17 23:56:52 +0100

    Add verify contract util

---
+++ hardhat/nft/backend/utils/verify.js
@@ -0,0 +1,24 @@
+// We can't have these functions in `helper-hardhat-config.js`
+// since these use the hardhat library
+// and it would be a circular dependency
+const { run } = require("hardhat");
+
+const verify = async (contractAddress, args) => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified!");
+    } else {
+      console.log(error);
+    }
+  }
+};
+
+module.exports = {
+  verify,
+};
