776d58299db15dd9afec01cc407f298c8d425b48
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-18 14:53:09 +0100

    Add missing deploy argument

--- hardhat/nft/backend/deploy/01-deploy-basic-nft.js
+++ hardhat/nft/backend/deploy/01-deploy-basic-nft.js
@@ -5,9 +5,8 @@ const { verify } = require("../utils/verify");
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
-
   const args = [];
-  const basicNft = await deploy({
+  const basicNft = await deploy("BasicNft", {
     from: deployer,
     args: args,
     log: true,
