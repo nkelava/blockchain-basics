17df43745b38c0c3a3fbad7b64e37d8cf1e8d931
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-20 19:49:26 +0100

    Update subscription id

--- hardhat/nft/backend/helper-hardhat-config.js
+++ hardhat/nft/backend/helper-hardhat-config.js
@@ -13,7 +13,7 @@ const networkConfig = {
     gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
     callbackGasLimit: "500000",
     mintFee: "10000000000000000",
-    subscriptionId: "1002",
+    subscriptionId: "10851",
   },
 };
 
