9c75200ed529f4674a10269c109ef912492ef4c9
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-21 02:00:27 +0100

    Add deploy random ipfs contract script

---
+++ hardhat/nft/backend/deploy/02-deploy-random-ipfs-nft.js
@@ -0,0 +1,62 @@
+const { network, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+const { handleTokenUris } = require("../utils/handleTokenURI");
+
+const FUND_AMOUNT = "1000000000000000000000";
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock;
+  let tokenUris = [
+    "ipfs://QmaVkBn2tKmjbhphU7eyztbvSQU5EXDdqRyXZtRhSGgJGo",
+    "ipfs://QmYQC5aGZu2PTH8XzbJrbDnvhj3gVs7ya33H9mqUNvST3d",
+    "ipfs://QmZYmH5iDbD6v3U2ixoVAjioSzvWJszDzYdbeCLquGSpVm",
+  ];
+
+  if (process.env.UPLOAD_TO_PINATA == "true") {
+    tokenUris = await handleTokenUris();
+  }
+
+  if (developmentChains.includes(network.name)) {
+    vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+
+    const txResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const txReceipt = await txResponse.wait(1);
+    subscriptionId = txReceipt.events[0].args.subId;
+    await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId].vrfCoordinatorV2;
+    subscriptionId = networkConfig[chainId].subscriptionId;
+  }
+
+  const args = [
+    vrfCoordinatorV2Address,
+    subscriptionId,
+    networkConfig[chainId].gasLane,
+    networkConfig[chainId].callbackGasLimit,
+    tokenUris,
+    networkConfig[chainId].mintFee,
+  ];
+
+  const randomIpfsNft = await deploy("RandomIpfsNft", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  if (developmentChains.includes(network.name)) {
+    await vrfCoordinatorV2Mock.addConsumer(subscriptionId, randomIpfsNft.address);
+  }
+
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...");
+    await verify(randomIpfsNft.address, args);
+  }
+};
+
+module.exports.tags = ["all", "randomipfs", "main"];
