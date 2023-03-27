6dc1ac029543a383ad23652765bc3426da676641
Nikola Kelava <nikola.kelava94@gmail.com>
2023-03-27 18:05:36 +0200

    Add nft marketplace contract deploy script

---
+++ hardhat/nft/marketplace/backend/deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,25 @@
+const { network } = require("hardhat");
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS;
+  const arguments = [];
+  const nftMarketplace = await deploy("NftMarketplace", {
+    from: deployer,
+    args: arguments,
+    log: true,
+    waitConfirmations: waitBlockConfirmations,
+  });
+
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    log("Verifying...");
+    await verify(nftMarketplace.address, arguments);
+  }
+};
+
+module.exports.tags = ["all", "nftmarketplace"];
